b5d831ee9207151e71bf9d44b9d8e10b82ba192a
Bucks, Acelle Kristia <acelle.kristia.bucks@accenture.com>
2021-09-03 19:59:28 +0800

    Adding files

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Main {
+    public static void main(String[] args) {
+
+int myArray [] = {10,20,30};
+int count=0;
+    for (int sample=0; sample<myArray.length; sample++) {
+     //  do{
+            System.out.println("This array in cell " + count + " has a value of " + myArray[sample]);
+        count++;
+       // }while(sample >count);
+    }
+
+    }
+}
